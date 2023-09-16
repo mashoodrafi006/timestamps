64d3c424c13573c2b8d9bedc8f12a049967cc1f5
Mashood <mashoodrafi006@gmail.com>
2023-09-16 18:23:48 +0500

    Add TimeController & first API route to get timestamps.

---
+++ app/Http/Controllers/TimeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class TimeController extends Controller {
+
+    public function calculateTimeDifferenceDetails(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $inputs = [
+            'first_timestamp' => $request->input('first_timestamp'),
+            'second_timestamp' => $request->input('second_timestamp'),
+            'expressions' => $request->input('expressions')
+        ];
+
+        return new \Illuminate\Http\JsonResponse(['message' => 'API that takes timestamps with expressions']);
+    }
+}
